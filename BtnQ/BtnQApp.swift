39a7328495a26c1b04c62092e7ec0f2a0e6e0572
Matt Sephton <matt.sephton@gmail.com>
2026-06-13 18:19:09 +0100

    Initial Commit

---
+++ BtnQ/BtnQApp.swift
@@ -0,0 +1,17 @@
+//
+//  BtnQApp.swift
+//  BtnQ
+//
+//  Created by Matt Sephton on 2026-06-13.
+//
+
+import SwiftUI
+
+@main
+struct BtnQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BtnQTests/BtnQTests.swift
@@ -0,0 +1,17 @@
+//
+//  BtnQTests.swift
+//  BtnQTests
+//
+//  Created by Matt Sephton on 2026-06-13.
+//
+
+import Testing
+@testable import BtnQ
+
+struct BtnQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
